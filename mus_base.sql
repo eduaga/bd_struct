26ac5a6e286265465477d25dee2d16b429baaed8
eduaga <agadzhanoff@mail.ru>
2020-10-20 13:15:13 +0300

    Update mus_base.sql

--- mus_base.sql
+++ mus_base.sql
@@ -1,27 +1,47 @@
-create table Genre (
+create table if not exists Musician (
+perf_id serial primary key,
+perf_name varchar(50)
+);
+
+create table if not exists Album (
+album_id serial primary key,
+album_title varchar(100),
+album_date integer
+);
+
+create table if not exists MusicianAlbum (
+mus_id integer references Musician(perf_id),
+album_id integer references Album(album_id),
+constraint ma primary key (mus_id, album_id)
+);
+
+create table if not exists Genre (
 genre_id serial primary key,
 genre_name varchar(40) unique
 );
 
-create table Musician (
-perf_id serial primary key,
-perf_name varchar(50),
-perf_genre integer,
-foreign key(perf_genre) references Genre(genre_id)
+create table if not exists MusicianGenre (
+mus_id integer references Musician(perf_id),
+genre_id integer references Genre(genre_id),
+constraint mg primary key (mus_id, genre_id)
 );
 
-create table Album (
-album_id serial primary key,
-album_title varchar(100),
-album_date integer,
-album_perf integer,
-foreign key(album_perf) references Musician(perf_id)
-);
-
-create table Track (
+create table if not exists Track (
 track_id serial primary key,
 track_title varchar(100),
 track_duration varchar(5),
 track_album integer,
 foreign key(track_album) references Album(album_id)
+);
+
+create table if not exists Collection (
+collect_id serial primary key,
+collect_name varchar(100),
+collect_date integer
+);
+
+create table if not exists TrackCollection (
+track_id integer references Track(track_id),
+collect_id integer references Collection(collect_id),
+constraint tc primary key (track_id, collect_id)
 );
